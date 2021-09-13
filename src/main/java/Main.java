2854ed96613d7520b79a03fdd173280949cfb05b
Geniuskaa <eminizinar@gmail.com>
2021-09-13 15:51:09 +0300

    2k-170 tests to Main class added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,30 +1,16 @@
 import java.util.InvalidPropertiesFormatException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvalidPropertiesFormatException {
 
-        try {
-            Parser.countOfArgumentsChecker(args);
-            var val1 = Parser.parseIntOrError(args[0]);
-            var operator = Parser.parseOperatorOrError(args[1]);
-            var val2 = Parser.parseIntOrError(args[2]);
-            int sum = new Calculator().calculate(val1, operator, val2);
-            System.out.println("Result is " + sum);
+        Parser.countOfArgumentsChecker(args);
 
-        }catch (InvalidPropertiesFormatException e) {
-            switch (e.getMessage()){
-                case("Argument is not number"):
-                    System.exit(1);
-                    break;
-                case("Operator is not supported"):
-                    System.exit(2);
-                    break;
-                case("Count of arguments is inncorrect"):
-                    System.exit(3);
-                    break;
-                default:
-                    System.exit(4);
-            }
-        }
+        var val1 = Parser.parseIntOrError(args[0]);
+        var operator = Parser.parseOperatorOrError(args[1]);
+        var val2 = Parser.parseIntOrError(args[2]);
+        int sum = new Calculator().calculate(val1, operator, val2);
+
+        System.out.println("Result is " + sum);
+
     }
 }
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,31 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.InvalidPropertiesFormatException;
+
+public class MainTest {
+
+    private String[] arguments;
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        this.arguments = new String[]{"4", "*", "3"};
+        System.setOut(new PrintStream(output));
+    }
+
+    @Test
+    public void mainTest() throws InvalidPropertiesFormatException {
+        Main.main(arguments);
+        Assert.assertEquals("Result is 12", output.toString().trim());
+    }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+}
