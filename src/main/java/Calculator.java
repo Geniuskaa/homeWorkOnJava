b092092ff188c97f0c9d394c89cc4ca531608237
Geniuskaa <eminizinar@gmail.com>
2021-09-13 16:54:42 +0300

    2k-170 fixed mistakes that noted reviewer

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,11 +1,19 @@
 public class Calculator {
-    public int calculate(int val1, String operator, int val2) {
-        return switch (operator) {
-            case "+" -> val1 + val2;
-            case "-" -> val1 - val2;
-            case "/" -> val1 / val2;
-            case "*" -> val1 * val2;
-            default -> 0;
-        };
+    public int calculate(int val1, String operator, int val2) throws ArithmeticException {
+        switch (operator) {
+            case "+" -> {return val1 + val2;}
+            case "-" -> {return val1 - val2;}
+            case "/" -> {
+                if (val1 == 0) {
+                    throw new ArithmeticException();
+                }
+                int sum = val1/val2;
+                return sum;
+
+
+            }
+            case "*" -> {return val1 * val2;}
+            default -> throw new ArithmeticException("It seems that operator isn`t correct!");
+        }
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 public class CalculatorTest {
     private Integer x;
@@ -17,27 +18,42 @@ public class CalculatorTest {
 
     @Test
     public void sum_X_Plus_Y_equal(){
-        Assert.assertEquals(10, calculator.calculate(x,"+", y));
+        Assert.assertEquals(x+y, calculator.calculate(x,"+", y));
     }
 
     @Test
     public void difference_X_Minus_Y_equal(){
-        Assert.assertEquals(-4, calculator.calculate(x,"-", y));
+        Assert.assertEquals(x-y, calculator.calculate(x,"-", y));
     }
 
     @Test
     public void amount_X_Multiply_Y_equal(){
-        Assert.assertEquals(21, calculator.calculate(x,"*", y));
+        Assert.assertEquals(x*y, calculator.calculate(x,"*", y));
     }
 
     @Test
     public void amount_X_Divide_Y_equal(){
-        Assert.assertEquals(0, calculator.calculate(x,"/", y));
+        Assert.assertEquals(x/y, calculator.calculate(x,"/", y));
+    }
+
+    @Test
+    public void divisionBy_0_Forbidden() throws ArithmeticException {
+        Assert.assertThrows(ArithmeticException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                calculator.calculate(0,"/",5);
+            }
+        });
     }
 
     @Test
     public void calculateWithWrongOperator(){
-        Assert.assertEquals(0, calculator.calculate(x,"%", y));
+        Assert.assertThrows(ArithmeticException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                calculator.calculate(2,"%",5);
+            }
+        });
     }
 
 
