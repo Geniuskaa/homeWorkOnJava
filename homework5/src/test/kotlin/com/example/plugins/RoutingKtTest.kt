bf743a2c8b31652d719c9b79f10cd1167f3be361
Geniuskaa <eminizinar@gmail.com>
2021-11-07 22:04:21 +0300

    Finished hw4

---
+++ homework5/src/test/kotlin/com/example/plugins/RoutingKtTest.kt
@@ -0,0 +1,12 @@
+package com.example.plugins
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class RoutingKtTest {
+
+    @Test
+    fun configureRouting() {
+    }
+}
