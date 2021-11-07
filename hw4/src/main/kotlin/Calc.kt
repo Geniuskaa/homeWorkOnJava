7d244c6113f914a652017390f7a282ffef536c44
Geniuskaa <eminizinar@gmail.com>
2021-11-07 22:18:33 +0300

    Removed homework5 folder

--- hw4/src/main/kotlin/Calc.kt
+++ hw4/src/main/kotlin/Calc.kt
@@ -4,7 +4,7 @@ import kotlinx.coroutines.runBlocking
 
 suspend fun main(args: Array<String>) {
     runBlocking {
-        val value = either<CalculatorException, Int> {
+        val value = either<CalculatorException, kotlin.Number> {
             numOfArgsChecker(args).bind()
             operatorChecker(args).bind()
             zeroDivisionChecker(args).bind()
@@ -29,14 +29,46 @@ sealed class CalculatorException {
     }
 }
 
-fun argumentIsInt(arg: String): Boolean {
-    return try {
-        Integer.parseInt(arg)
-        true
-    } catch (e: NumberFormatException) {
-        false
+
+fun argumentIsNum(arg: String): kotlin.Number? {
+
+    try {
+        val a = arg.toInt();
+        return a
+    }catch (e: NumberFormatException){ }
+
+    try {
+        val b = arg.toFloat()
+        return b
+    }catch (e: NumberFormatException){ }
+
+    try {
+        val c = arg.toDouble();
+        return c
+    }catch (e: NumberFormatException){ }
+
+    return null
+}
+
+
+private fun first_arg_converter(arg: kotlin.Number?): Number {
+    return when (arg) {
+        is Int -> return Number(arg)
+        is Float -> return Number(arg)
+        is Double -> return Number(arg)
+        else -> return throw NumberFormatException("Something gone wrong")
     }
 }
+
+private fun second_arg_converter(arg: kotlin.Number?): kotlin.Number {
+    return when (arg) {
+        is Int -> return arg
+        is Float -> return arg
+        is Double -> return arg
+        else -> return throw NumberFormatException("Something gone wrong")
+    }
+}
+
 
 fun numOfArgsChecker(args: Array<String>): Either<CalculatorException.WrongNumOfArgs, Array<String>> =
     if (args.size != 3) Either.Left(CalculatorException.WrongNumOfArgs)
@@ -50,13 +82,79 @@ fun zeroDivisionChecker(args: Array<String>): Either<CalculatorException.ZeroDiv
     if (args[1] == "/" && args[2] == "0") Either.Left(CalculatorException.ZeroDivision)
     else Either.Right(args)
 
-fun calculate(args: Array<String>): Either<CalculatorException.WrongArgument, Int> =
-    if (!argumentIsInt(args[0]) || !argumentIsInt(args[2])) Either.Left(CalculatorException.WrongArgument)
-    else when (args[1]) {
-        "+" -> Either.Right(Integer.parseInt(args[0]) + Integer.parseInt(args[2]))
-        "-" -> Either.Right(Integer.parseInt(args[0]) - Integer.parseInt(args[2]))
-        "/" -> Either.Right(Integer.parseInt(args[0]) / Integer.parseInt(args[2]))
-        "*" -> Either.Right(Integer.parseInt(args[0]) * Integer.parseInt(args[2]))
-        else -> Either.Left(CalculatorException.WrongArgument)
+fun calculate(args: Array<String>): Either<CalculatorException.WrongArgument, kotlin.Number> {
+    return try {
+
+        when (args[1]) {
+            "+" -> Either.Right(first_arg_converter(argumentIsNum(args[0])) + second_arg_converter(argumentIsNum(args[2])))
+            "-" -> Either.Right(first_arg_converter(argumentIsNum(args[0])) - second_arg_converter(argumentIsNum(args[2])))
+            "/" ->if(args[2].equals("0")) throw NumberFormatException() else Either.Right(first_arg_converter(argumentIsNum(args[0])) / second_arg_converter(argumentIsNum(args[2])))
+            "*" -> Either.Right(first_arg_converter(argumentIsNum(args[0])) * second_arg_converter(argumentIsNum(args[2])))
+            else -> Either.Left(CalculatorException.WrongArgument)
+        }
+    } catch (e: NumberFormatException) {
+        return Either.Left(CalculatorException.WrongArgument)
     }
+}
+
+data class Number(val f_arg: kotlin.Number){
+
+    operator fun plus(s_arg: kotlin.Number): kotlin.Number{
+        when(f_arg){
+            is Int -> when(s_arg){
+                is Int -> return f_arg.toInt() + s_arg.toInt()
+                else -> return f_arg.toInt() + s_arg.toDouble()
+            }
+            else -> when(s_arg){
+                is Int -> return f_arg.toDouble() + s_arg.toInt()
+                else -> return f_arg.toDouble() + s_arg.toDouble()
+            }
+        }
+    }
+
+    operator fun minus(s_arg: kotlin.Number): kotlin.Number{
+        when(f_arg){
+            is Int -> when(s_arg){
+                is Int -> return f_arg.toInt() - s_arg.toInt()
+                else -> return f_arg.toInt() - s_arg.toDouble()
+            }
+            else -> when(s_arg){
+                is Int -> return f_arg.toDouble() - s_arg.toInt()
+                else -> return f_arg.toDouble() - s_arg.toDouble()
+            }
+        }
+    }
+
+    operator fun div(s_arg: kotlin.Number): kotlin.Number{
+        when(f_arg){
+            is Int -> when(s_arg){
+                is Int -> return f_arg.toInt() / s_arg.toInt()
+                else -> return f_arg.toInt() / s_arg.toDouble()
+            }
+            else -> when(s_arg){
+                is Int -> return f_arg.toDouble() / s_arg.toInt()
+                else -> return f_arg.toDouble() / s_arg.toDouble()
+            }
+        }
+    }
+
+    operator fun times(s_arg: kotlin.Number): kotlin.Number{
+        when(f_arg){
+            is Int -> when(s_arg){
+                is Int -> return f_arg.toInt() * s_arg.toInt()
+                else -> return f_arg.toInt() * s_arg.toDouble()
+            }
+            else -> when(s_arg){
+                is Int -> return f_arg.toDouble() * s_arg.toInt()
+                else -> return f_arg.toDouble() * s_arg.toDouble()
+            }
+        }
+    }
+}
+
+
+
+
+
+
 
